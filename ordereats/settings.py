2edce048361c88e2a9e12c383e4d2f9fa741ad9e
Abhiuday <contact.abhiuday@gmail.com>
2024-06-15 20:14:36 +0530

    fix(ordereats): fixed settings
    Signed-off-by: Abhiuday <contact.abhiuday@gmail.com>

--- ordereats/settings.py
+++ ordereats/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ordereats.onrender.com']
 
 
 # Application definition
